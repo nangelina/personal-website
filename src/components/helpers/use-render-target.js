1757fe4d82feb0fd48f09ba23d1934d79a510f09
Angelina Nguyen <nangelina1412@gmail.com>
2022-06-02 00:31:38 +1000

    update to @react-three/fiber package

--- src/components/helpers/use-render-target.js
+++ src/components/helpers/use-render-target.js
@@ -1,6 +1,6 @@
 import { useMemo, useRef } from 'react';
 import * as THREE from 'three';
-import { useFrame } from 'react-three-fiber';
+import { useFrame } from '@react-three/fiber';
 
 function useRenderTarget(settings = {}) {
   const renderTarget = useMemo(() => {

87143d6b2f0fb96ae77b2be57096e083719683ae
Angelina Nguyen <nangelina1412@gmail.com>
2022-06-01 23:31:58 +1000

    fix alligator demo

--- src/components/AlligatorDemo/Cubes.jsx
+++ src/components/AlligatorDemo/Cubes.jsx
@@ -1,79 +1,19 @@
-import React, {
-  useMemo,
-  useRef,
-  useState,
-  useEffect,
-  useCallback,
-} from 'react';
-import { random } from 'lodash';
+import React, { useRef } from 'react';
+import { map } from 'lodash';
 import { useFrame } from '@react-three/fiber';
 
+import Cube from './Cube';
+
 export default () => {
-  const mesh = useRef();
-  const time = useRef(0);
+  const group = useRef();
 
-  const [isHovered, setIsHovered] = useState(false);
-  const [isActive, setIsActive] = useState(false);
-
-  const isActiveRef = useRef(isActive);
-
-  // position
-  const position = useMemo(() => {
-    return [random(-3, 3, true), random(-3, 3, true), random(-3, 3, true)];
-  }, []);
-
-  // random time mod factor
-  const timeMod = useMemo(() => random(0.1, 4, true), []);
-
-  // color
-  const color = isHovered ? 0xe5d54d : isActive ? 0xf7e7e5 : 0xf95b3c;
-
-  //useEffect of the activeState
-  useEffect(() => {
-    isActiveRef.current = isActive;
-  }, [isActive]);
-
-  // raf loop
   useFrame(() => {
-    mesh.current.rotation.y += 0.01 * timeMod;
-    if (isActiveRef.current) {
-      time.current += 0.03;
-      mesh.current.position.y = position[1] + Math.sin(time.current) * 0.4;
-    }
+    group.current.rotation.y += 0.005;
   });
 
-  // Events
-  const onHover = useCallback(
-    (e, value) => {
-      e.stopPropagation();
-      setIsHovered(value);
-    },
-    [setIsHovered]
-  );
+  const nodesCubes = map(new Array(50), (el, i) => {
+    return <Cube key={i} />;
+  });
 
-  const onClick = useCallback(
-    (e) => {
-      e.stopPropagation();
-      setIsActive((v) => !v);
-    },
-    [setIsActive]
-  );
-
-  return (
-    <mesh
-      ref={mesh}
-      position={position}
-      onClick={(e) => onClick(e)}
-      onPointerOver={(e) => onHover(e, true)}
-      onPointerOut={(e) => onHover(e, false)}
-    >
-      <boxBufferGeometry attach="geometry" args={[0.047, 0.5, 0.29]} />
-      <meshStandardMaterial
-        attach="material"
-        color={color}
-        roughness={0.6}
-        metalness={0.1}
-      />
-    </mesh>
-  );
+  return <group ref={group}>{nodesCubes}</group>;
 };

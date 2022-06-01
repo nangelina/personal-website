73b45f7a72a453ce82d5627dd311cd6bb9d7922b
Angelina Nguyen <nangelina1412@gmail.com>
2022-06-02 01:15:23 +1000

    mirrors have a reflection but no text

--- src/components/Mirrors/Mirror.jsx
+++ src/components/Mirrors/Mirror.jsx
@@ -5,25 +5,8 @@ import { Box } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 
 import useLayers from '../helpers/use-layers';
-// import { ThinFilmFresnelMap } from '../helpers/ThinFilmFresnelMap';
 
-// const thinFilmFresnelMap = new ThinFilmFresnelMap();
-
-// function SideMaterial() {
-//   return <meshLambertMaterial map={thinFilmFresnelMap} color="#AAAAAA" />;
-// }
-
-// function ReflectionMaterial({ envMap }) {
-//   return (
-//     <meshLambertMaterial
-//       map={thinFilmFresnelMap}
-//       envMap={envMap}
-//       color="#FFFFFF"
-//     />
-//   );
-// }
-
-function Mirror({ envMap, layers, ...props }) {
+function Mirror({ SideMaterial, ReflectionMaterial, layers, ...props }) {
   const ref = useLayers(layers);
 
   useFrame(() => {
@@ -35,8 +18,12 @@ function Mirror({ envMap, layers, ...props }) {
 
   return (
     <Box {...props} ref={ref}>
-      {/* <SideMaterial />
-      <ReflectionMaterial envMap={envMap} /> */}
+      <SideMaterial />
+      <SideMaterial />
+      <SideMaterial />
+      <SideMaterial />
+      <ReflectionMaterial />
+      <ReflectionMaterial />
     </Box>
   );
 }
--- src/components/Mirrors/Mirrors.jsx
+++ src/components/Mirrors/Mirrors.jsx
@@ -1,11 +1,37 @@
+import { useCallback } from 'react';
+
 // import PropTypes from 'prop-types';
 // import { Texture } from '@types/three';
 
 import { mirrorsData } from '../helpers/data';
+// import { ThinFilmFresnelMap } from '../helpers/ThinFilmFresnelMap';
 
 import Mirror from './Mirror';
 
+// const thinFilmFresnelMap = new ThinFilmFresnelMap();
+
 function Mirrors({ envMap, layers, ...props }) {
+  const SideMaterial = useCallback(
+    () => (
+      <meshLambertMaterial
+        // map={thinFilmFresnelMap}
+        color="#AAAAAA"
+      />
+    ),
+    []
+  );
+
+  const ReflectionMaterial = useCallback(
+    () => (
+      <meshLambertMaterial
+        // map={thinFilmFresnelMap}
+        envMap={envMap}
+        color="#FFFFFF"
+      />
+    ),
+    [envMap]
+  );
+
   return (
     <group name="mirrors" {...props}>
       {mirrorsData.mirrors.map((mirror, index) => (
@@ -14,7 +40,8 @@ function Mirrors({ envMap, layers, ...props }) {
           layers={layers}
           {...mirror}
           name={`mirror-${index}`}
-          envMap={envMap}
+          SideMaterial={SideMaterial}
+          ReflectionMaterial={ReflectionMaterial}
         />
       ))}
     </group>
